b084569c667a0a6cd2fd9b8de724794bdff20df8
wayne0830 <wayne0830@163.com>
2015-04-15 23:45:08 +0800

    add setting script

--- runme.sh
+++ runme.sh
@@ -3,9 +3,13 @@
 selfpath=$(cd $(dirname $0); pwd)
 sys_name=$1
 
+if [ "$sys_name" != "fedora" -a "$sys_name" != "macos" -a "$sys_name" != "opensuse" ]; then
+    exit
+fi
+
 ln -s $selfpath/$sys_name/.inputrc $HOME/.inputrc
 ln -s $selfpath/$sys_name/.vimrc $HOME/.vimrc
 ln -s $selfpath/vim $HOME/.vim
 
-echo -e "\n" >> $HOME/.bashrc
-echo "source $selfpath/$sys_name/.bashrc"
+echo -e "\n" >> $HOME/.bash_profile
+echo "source $selfpath/$sys_name/.bashrc" >> $HOME/.bash_profile
