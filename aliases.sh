b3c374369f9c6feda5c2bbab18e3df24484a8fe8
wayne0830 <wayne0830@163.com>
2015-04-22 11:14:34 +0800

    a

--- aliases.sh
+++ aliases.sh
@@ -1,14 +1,15 @@
 if [ -x /usr/bin/dircolors ]; then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-    alias ls='ls --color=auto'
-    alias dir='dir --color=auto'
-    alias vdir='vdir --color=auto'
-
-    alias grep='grep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
-    alias igrep='grep -i --color=auto'
 fi
+
+alias ls='ls --color=auto'
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias igrep='grep -i --color=auto'
 
 alias ll='ls -lFh'
 alias lla='ls -lFhA'
