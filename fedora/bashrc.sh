85d6d1af1458eb801c256791bc78529beff3b577
wayne0830 <wayne0830@163.com>
2015-06-10 09:29:18 +0800

    change PS1

--- fedora/bashrc.sh
+++ fedora/bashrc.sh
@@ -1,4 +1,4 @@
-PS1="$BLUE\u$BCYAN@${YELLOW}linux $PURPLE[$GREEN"'$(pwd | sed "s/^\/\([^\/]\+\)\/.*\/\([^\/]\+\)\/\([^\/]\+\)\/\([^\/]*\)$/\/\1\/...\/\3\/\4/")'"$PURPLE]"'$(test -n "$(git branch 2>/dev/null)" && echo " [")'"$BYELLOW"'$(git branch 2>/dev/null | grep "^*" | cut -d " " -f 2,2)'"$PURPLE"'$(test -n "$(git branch 2>/dev/null)" && echo "]")'" $BCYAN)>$RESET "
+PS1="$BLUE\u$BCYAN@${YELLOW}linux $PURPLE[$GREEN"'$(pwd | sed "s/^\/\([^\/]\+\)\/.*\/\([^\/]\+\)\/\([^\/]\+\)\/\([^\/]*\)$/\/\1\/ ... \/\3\/\4/")'"$PURPLE]"'$(test -n "$(git branch 2>/dev/null)" && echo " [")'"$BYELLOW"'$(git branch 2>/dev/null | grep "^*" | cut -d " " -f 2,2)'"$PURPLE"'$(test -n "$(git branch 2>/dev/null)" && echo "]")'" $BCYAN)>$RESET "
 
 # for root
 #PS1="\[\e[31m\]\u \[\e[35m\][\[\e[32m\]\W\[\e[35m\]] \[\e[1;36m\]\$>\[\e[0m\] "
