24d6179b0798688acb1c41506e9213c36825c557
wayne0830 <wayne0830@163.com>
2015-07-02 16:46:42 +0800

    a

--- common_bashrc.sh
+++ common_bashrc.sh
@@ -14,7 +14,7 @@ RESET="\[\e[0m\]"
 mcd() { mkdir -p "$1"; cd "$1"; }
 
 pd() {
-    if [[ "$1" =~ ^[0-9+]$ ]]; then
+    if [[ "$1" =~ ^[0-9]+$ ]]; then
         target_dir=".."
         for (( i=2; i<=$1; i++ )); do
             target_dir="${target_dir}/.."
