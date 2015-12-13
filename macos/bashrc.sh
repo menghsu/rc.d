4130357d3237ff618a6038d1c360e07dc029a08c
wayne0830 <wayne0830@163.com>
2015-12-14 00:15:13 +0800

    bash_profile of mac

--- macos/bashrc.sh
+++ macos/bashrc.sh
@@ -1,3 +1,4 @@
+# linux like commands
 #if brew list | grep coreutils > /dev/null; then
 #    export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 #    alias ls='ls -F --show-control-chars --color=auto'
@@ -6,6 +7,13 @@
 
 if [ -r $HOME/.bin ]; then
     export PATH="$PATH:$HOME/.bin"
+fi
+
+if type brew>/dev/null 2>&1; then
+    brew_prefix=$(brew --prefix)
+    if [ -f $brew_prefix/etc/bash_completion ]; then
+        source $brew_prefix/etc/bash_completion
+    fi
 fi
 
 export CLICOLOR=1
